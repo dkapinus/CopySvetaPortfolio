7070220f70a6136ac5f6af0b0c0d7a6d630c3f58
dkapinus <dimonio@tut.by>
2023-11-28 11:45:56 +0300

    add Burger menu

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Menu} from "layout/header/headerMenu/menu/Menu";
 import {MenuPropsType} from "layout/header/headerMenu/desktopMenu/DesktopMenu";
-import {S} from  "layout/header/headerMenu/HeaderMenu_Styles"
-
-
+import {S} from "layout/header/headerMenu/HeaderMenu_Styles"
 
 
 export const MobileMenu: React.FC<MenuPropsType> = ({item, ...props}) => {
 
+    const [menuIsOpen, setIsOpen] = useState(false)
+
+    const onBurgerBtnClick = () => {
+         setIsOpen(!menuIsOpen)
+    }
     return (
         <S.MobileMenu>
-            <S.BurgerButton isOpen={false}><span></span></S.BurgerButton>
-            <S.MobileMenuPopup isOpen={false}>
-               <Menu item={item}/>
+            <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}><span></span></S.BurgerButton>
+            <S.MobileMenuPopup isOpen={menuIsOpen}>
+                <Menu item={item}/>
             </S.MobileMenuPopup>
 
 
