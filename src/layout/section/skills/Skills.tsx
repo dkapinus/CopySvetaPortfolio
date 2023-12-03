f8373f19c96296cc4f46279b528b1f08904af690
dkapinus <dimonio@tut.by>
2023-12-03 11:46:18 +0300

    React-awesome-reveal

--- src/layout/section/skills/Skills.tsx
+++ src/layout/section/skills/Skills.tsx
@@ -4,44 +4,57 @@ import {StyledSectionTitle} from "components/StyledTitileLevelTwo";
 import {Skill} from "layout/section/skills/skill/Skill";
 import {Container} from "components/Container";
 import {S} from "layout/section/skills/SkillsStyled"
-
+import {Fade} from "react-awesome-reveal";
 
 
 const SkillsData = [
-    {id:"codeSvg",
-        title:"html5"},
-    {id:"css",
-        title:"css3"},
-    {id:"react",
-        title:"React"},
-    {id:"typescript",
-        title:"typescript"},
-    {id:"styledComponents",
-        title:"styled components"},
-    {id:"figma",
-        title:"figma"},
+    {
+        id: "codeSvg",
+        title: "html5"
+    },
+    {
+        id: "css",
+        title: "css3"
+    },
+    {
+        id: "react",
+        title: "React"
+    },
+    {
+        id: "typescript",
+        title: "typescript"
+    },
+    {
+        id: "styledComponents",
+        title: "styled components"
+    },
+    {
+        id: "figma",
+        title: "figma"
+    },
 
 
 ]
 
 
-
-export const Skills:React.FC = () => {
+export const Skills: React.FC = () => {
     return (
         <S.Skills id={"skills"}>
             <Container>
                 <StyledSectionTitle>My Skills</StyledSectionTitle>
 
                 <FlexWrapper wrap={"wrap"} justify={"space-between"}>
-
-                    {SkillsData.map((el,index)=> {
-                      return (
-                          <Skill title={el.title} id={el.id} text={"HyperText Markup Language (HTML) is a" +
-                              " markup language used to create web pages. It was created in 1990 by Sir Tim Berners-Lee, a British " +
-                              "computer scientist who is credited with inventing the World Wide Web."} key={index}/>
-                      )
-                    })}
-
+                    <Fade cascade={true}>
+                        damping={0.2}
+                        {SkillsData.map((el, index) => {
+                            return (
+                                <Skill title={el.title} id={el.id} text={"HyperText Markup Language (HTML) is a" +
+                                    " markup language used to create web pages. It was created in 1990 by Sir Tim Berners-Lee, a British " +
+                                    "computer scientist who is credited with inventing the World Wide Web."}
+                                       key={index}/>
+                            )
+                        })}
+                    </Fade>
                 </FlexWrapper>
             </Container>
 
